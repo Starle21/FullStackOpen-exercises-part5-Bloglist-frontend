a4550807bb6818c737047e1fc9fa04ccae0672c4
Starle21 <staronova.lenka@gmail.com>
2021-07-17 21:00:02 +0200

    Part 5.7* Bloglist frontend, step7 - blog togglable visibility

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,26 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const hiddenDefault = { display: visible ? "" : "none" };
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  return (
+    <div className="blog">
+      {/* when details hidden */}
+      {blog.title} {blog.author}{" "}
+      <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+      {/*  when details visible*/}
+      <div style={hiddenDefault}>
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
